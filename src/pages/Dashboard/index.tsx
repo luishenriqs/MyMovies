0e7a38dd216d68a12e62194878c475bbd2891389
Luis Henrique Pereira <lh.p@hotmail.com>
2020-11-07 18:21:50 -0300

    First view in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/header/index';
+import api from '../../services/api';
+import GettingDates from '../../utils/gettingDates';
+
+interface Repository {
+  title: string;
+  overview: string;
+  poster_path: string;
+  original_language: string;
+}
 
 const Dashboard: React.FC = () => {
-  return <Header />;
+  const [repository, setRepository] = useState<Repository[]>([]);
+
+  /* ****************************[API_KEY]*********************************** */
+  const api_key = '2964b6cd71e6a379510ab626bdca951e';
+  /* ************************************************************************ */
+
+  /* *********************[Capturando últimos 30 dias]*********************** */
+  const { initialDate } = GettingDates();
+  const { todayDate } = GettingDates();
+  /* ************************************************************************ */
+
+  /* ********************[Consumindo API PÚBLICA TMDB]*********************** */
+  // Listagem dos filmes mais populares dos últimos 30 dias;
+  useEffect(() => {
+    api
+      .get(
+        `discover/movie?api_key=${api_key}&primary_release_date.gte=${initialDate}&primary_release_date.lte=${todayDate}&language=pt-BR&sort_by=popularity.desc`,
+      )
+      .then(response => {
+        setRepository(response.data);
+      });
+  }, []);
+
+  const repositoryObj = Object.values(repository);
+  const movies = Object.values(repositoryObj[3]);
+  console.log(movies[1]);
+  console.log(movies[1].title);
+  /* ************************************************************************ */
+  return (
+    <>
+      <Header />
+      <h1>{movies[1].title}</h1>
+      <h1>{movies[1].overview}</h1>
+      <h1>{movies[1].release_date}</h1>
+    </>
+  );
 };
 export default Dashboard;
---
+++ src/utils/gettingDates.ts
@@ -0,0 +1,18 @@
+import { format, sub } from 'date-fns';
+
+const GettingDates = () => {
+  const todayDate = format(new Date(), 'yyyy-MM-dd');
+  const result = sub(new Date(), {
+    years: 0,
+    months: 0,
+    weeks: 0,
+    days: 30,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+  const initialDate = format(new Date(result), 'yyyy-MM-dd');
+  return { initialDate, todayDate };
+};
+
+export default GettingDates;
